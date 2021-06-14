f2b176c4a11c5b4ff16cb2e88b1b9228056bac7d
Mitsu <dizaztor@outlook.com>
2021-06-15 00:20:04 +0200

    test

---
+++ main.py
@@ -0,0 +1,10 @@
+from loguru import logger
+from bot.dispatcher import updater, jobq
+
+logger.add(
+    "logs/{time}.log",
+    rotation='00:00')
+updater.start_polling()
+jobq.start()
+logger.info("Doragon is now running")
+updater.idle()
